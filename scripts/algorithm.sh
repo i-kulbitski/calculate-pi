d41236b6401098666d4d58821dabbd19ab91b837
Ilya Kulbitski <ilya.kulbitski@n-able.com>
2024-09-13 17:55:07 +0200

    test

--- scripts/algorithm.sh
+++ scripts/algorithm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # UNCOMMENT THE FOLLOWING LINE TO GET A MORE PRECISE CALCULATION OF PI. ALSO REMOVE THE PREVIOUS ECHO STATEMENTS.
-echo "This is Pi, no kidding:"
+echo "This is Pi, no kidding, this time for sure:"
 echo "scale=1000; 4*a(1)" | bc -l | tee report.txt
